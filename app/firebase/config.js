942d3f2aa4d50fe64cc3fcda477853e0e23b602f
UnknownRider <barikpriyam28@gmail.com>
2024-06-05 05:39:18 +0530

    Firebase Issue

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore'; // Ensure you import getFirestore
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCLBrOUX2nItYZTTYzZuafP-RXcOo-Uk0Y",
+  authDomain: "intern-assignment-149f8.firebaseapp.com",
+  projectId: "intern-assignment-149f8",
+  storageBucket: "intern-assignment-149f8.appspot.com",
+  messagingSenderId: "807497612125",
+  appId: "1:807497612125:web:7747ac0983f60c1035c6b4",
+  measurementId: "G-DQSDF7RVKZ"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
